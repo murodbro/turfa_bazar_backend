9afe744e512ad8aa3bb5e506b586d6a2171378c8
murodbro <murodjon.work@gmail.com>
2024-08-25 16:34:48 +0500

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-# Ensure that pip is available
-if ! command -v pip &> /dev/null
-then
-    echo "pip could not be found. Installing pip..."
-    apt-get update && apt-get install -y python3-pip
-fi
-
-# Install Python dependencies
+#!/bin/sh
 pip install -r requirements.txt
-
-# Run Django collectstatic
 python3.9 manage.py collectstatic --noinput
