af42a7bb12e182eefa40a40ff18244fc8e67667c
murodbro <murodjon.work@gmail.com>
2024-08-25 17:23:31 +0500

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
-# Collect static files
 python manage.py collectstatic --noinput
